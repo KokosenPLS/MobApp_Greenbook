bab730bb19e1e6b4b0695b78655ef328dcc6652b
Alexander Longum Olsen <alexanderlongumolsen@gmail.com>
2021-09-24 13:01:16 +0200

    Testet med Meny, fikk det ikke til så bra

--- app/src/main/java/com/example/gui3/FeedActivity.kt
+++ app/src/main/java/com/example/gui3/FeedActivity.kt
@@ -2,10 +2,15 @@ package com.example.gui3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class FeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
     }
 }
