c14b249f8345e74660898c5796fb611a001e86f0
TrulsHafnor <58667703+TrulsHafnor@users.noreply.github.com>
2021-10-06 23:16:10 +0200

    Ferdig

--- app/src/main/java/com/example/gui3/ArrangementFragment.kt
+++ app/src/main/java/com/example/gui3/ArrangementFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 
 class ArrangementFragment : Fragment() {
@@ -14,17 +15,31 @@ class ArrangementFragment : Fragment() {
 
         activity?.findViewById<TextView>(R.id.toolbar_tittel)?.text = "Arrangement tittel"
 
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_arrangement, container, false)
 
     }
 
     companion object {
         fun newInstance(param1: String, param2: String) = ArrangementFragment()
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val btnPaameldte = view?.findViewById<Button>(R.id.btnPaameldte)
+        btnPaameldte?.setOnClickListener {
+            btnPaameldte.text="trykket"
+            val transaction = activity?.supportFragmentManager?.beginTransaction()
+            transaction?.replace(R.id.fragContainerUser,DeltakereArrangement())
+            transaction?.commit()
+        }
     }
 }
