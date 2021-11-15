5393b5d1db86ece4e392d0f1457fa99f470b8c05
TrulsHafnor <58667703+TrulsHafnor@users.noreply.github.com>
2021-11-15 18:25:55 +0100

    Nå er profil satt opp

--- app/src/main/java/com/example/greenbook/activities/MainActivity.kt
+++ app/src/main/java/com/example/greenbook/activities/MainActivity.kt
@@ -1,32 +1,38 @@
 package com.example.greenbook.activities
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract.Helpers.update
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
-import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.*
+import com.example.greenbook.Database
 import com.example.greenbook.NavGraphDirections
 import com.example.greenbook.R
-import com.example.greenbook.fragments.ArrangementFragment
+import com.example.greenbook.dataObjekter.Profil
 import com.example.greenbook.fragments.LoggInnFragmentDirections
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
-
+    private lateinit var database: Database
     private lateinit var auth: FirebaseAuth
 
     private var loggedIn = false // Fiksa en bug når appen kom tilbake fra velge bilde activity
@@ -36,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         auth = Firebase.auth
+        database = Database()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
@@ -48,7 +55,6 @@ class MainActivity : AppCompatActivity() {
         val nav_view = findViewById<NavigationView>(R.id.nav_view)
         nav_view.setupWithNavController(navController)
 
-        // TODO: 10/27/2021 Truls: Denne har jeg lagt til for å kunne navigere seg 
         nav_view.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.nav_nytt_arrangement -> {
@@ -70,7 +76,8 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 R.id.nav_account -> {
-
+                    val action =  NavGraphDirections.actionGlobalProfilFragment(auth.uid.toString(), "et navn")
+                    navController.navigate(action)
                     drawerLayout.closeDrawer(GravityCompat.START)
                     true
                 }
@@ -125,5 +132,22 @@ class MainActivity : AppCompatActivity() {
         finish()
         startActivity(intent)
     }
+
+    private fun getProfil(){
+        val profilListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val profil = snapshot.getValue<Profil>()!!
+                update(profil!!)
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+        }
+        database.database.child("bruker").child(auth.uid.toString()).addValueEventListener(profilListener)
+    }
+
+    private fun update(profil: Profil){
+        // TODO: 11/15/2021 legg inn infor til navgraph og fiks så navnet i selve navgrap pluss email blir endret
+    }
+
 
 }
