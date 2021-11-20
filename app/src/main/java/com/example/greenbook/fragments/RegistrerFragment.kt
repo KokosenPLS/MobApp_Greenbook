ae0508c1f8abb840f7fe34a18a95f4516d187ac6
Alexander Longum Olsen <alexanderlongumolsen@gmail.com>
2021-11-20 19:12:20 +0100

    Oppdatert registreringsside

--- app/src/main/java/com/example/greenbook/fragments/RegistrerFragment.kt
+++ app/src/main/java/com/example/greenbook/fragments/RegistrerFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import com.example.greenbook.Database
 import com.example.greenbook.R
 import com.example.greenbook.dataObjekter.Profil
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -19,17 +20,18 @@ import java.time.format.DateTimeFormatter
 import java.util.*
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.util.regex.Pattern
 
 class RegistrerFragment : Fragment(R.layout.fragment_registrer) {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var database: Database
 
-    private lateinit var email: EditText
-    private lateinit var passord: EditText
-    private lateinit var passord2: EditText
-    private lateinit var fornavn: EditText
-    private lateinit var etternavn: EditText
+    private lateinit var email: TextInputLayout
+    private lateinit var passord: TextInputLayout
+    private lateinit var passordBekreft: TextInputLayout
+    private lateinit var fornavn: TextInputLayout
+    private lateinit var etternavn: TextInputLayout
     private lateinit var fdato: TextView
     private lateinit var datoKnapp: ImageView
 
@@ -43,118 +45,163 @@ class RegistrerFragment : Fragment(R.layout.fragment_registrer) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        email = view?.findViewById(R.id.txt_email)
-        passord = view?.findViewById(R.id.registrer_bruker_passord_1)
-        passord2 = view?.findViewById(R.id.registrer_bruker_passord_2)
-        fornavn = view?.findViewById(R.id.txt_tittel)
-        etternavn = view?.findViewById(R.id.txt_etternavn)
+        email = view?.findViewById(R.id.register_input_email)
+        passord = view?.findViewById(R.id.register_input_passord)
+        passordBekreft = view?.findViewById(R.id.register_input_passord_confirm)
+        fornavn = view?.findViewById(R.id.register_input_fornavn)
+        etternavn = view?.findViewById(R.id.register_input_etternavn)
 
-        datepicker()
+        datepicker(view)
 
-        view?.findViewById<Button>(R.id.btn_registrer).setOnClickListener {
-                if(email.text.isEmpty()){
-                    email.setError(resources.getString(R.string.tomEpost_error))
-                    //Denne er litt bugga/funker ikke
-                }else if(isEmailValid(email.toString()))
-                        email.setError(resources.getString(R.string.validitetEpost_error))
-                        Log.i("Key",email.toString())
-
-                if(passord.text.isEmpty()){
-                    passord.setError(resources.getString(R.string.tomPassord_error))
-                }
-                if(passord2.text.isEmpty()){
-                    passord2.setError(resources.getString(R.string.tomPassord_error))
-                }
-                if(fornavn.text.isEmpty()){
-                    fornavn.setError(resources.getString(R.string.tomFornavn_error))
-                }
-                if(etternavn.text.isEmpty()){
-                    etternavn.setError(resources.getString(R.string.tomEtternavn_error))
-                }
-                if(fdato.text.isEmpty()){
-                    fdato.setHint(resources.getString(R.string.tomFDato_error))
-                    fdato.setHintTextColor(resources.getColor(R.color.error))
-                }
-
-                if(passord.text != null
-                    && passord2.getText().toString().equals(passord.getText().toString())
-                    && fornavn.text.isEmpty() == false
-                    && etternavn.text.isEmpty() == false
-                    && fdato.text.isEmpty() == false
-                    && Patterns.EMAIL_ADDRESS.matcher(email.text).matches()){
-                    auth.createUserWithEmailAndPassword(email.text.toString(), passord.text.toString())
-                                .addOnCompleteListener(requireActivity()) { task ->
-                                    if (task.isSuccessful) {
-                                        val user = auth.currentUser
-                                        val bruker = Profil(
-                                            email.text.toString(),
-                                            fornavn.text.toString(),
-                                            etternavn.text.toString(),
-                                            fdato.text.toString()
-                                        )
-                                        database.addBruker(user?.uid!!, bruker)
-                                        reload()
-                                    } else{
-                                        Toast.makeText(
-                                            context,
-                                            "Autentisering feilet: ${task.exception.toString()}",
-                                            Toast.LENGTH_LONG
-                                        ).show()
-                                    }
-                                }
-                        }
-                }
+        view.findViewById<Button>(R.id.btn_registrer).setOnClickListener {
+            validateInput()
+        }
     }
 
-    fun datepicker() {
-        datoKnapp = view?.findViewById(R.id.registrer_bruker_dp_icon)!!
-        fdato = view?.findViewById(R.id.registrer_bruker_datepicker)!!
+    fun validateEmail(): Boolean{
+        val emailInput = email.editText?.text.toString().trim()
+        if(emailInput.isEmpty()){
+            email.error = "Epost kan ikke være tom"
+            return false
+        }
+        else if(!isEmailValid(emailInput)){
+            email.error = "Ugyldig epost"
+            return false
+        }
+        else{
+            email.error = null
+            return true
+        }
+    }
+
+    fun validatePassord(): Boolean{
+        val passordInput = passord.editText?.text.toString().trim()
+        val passordInputBekreft = passordBekreft.editText?.text.toString().trim()
+        val passwordREGEX = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\\\S+\$)")
+        if(passordInput.isEmpty()){
+            passord.error = "Passord kan ikke være tomt"
+            return false
+        }
+        else if(passordInput.length < 6){
+            passord.error = "Passord må ha 6 eller flere karakterer"
+            return false
+        }/*
+        else if (!passwordREGEX.matcher(passordInput).matches()){
+            Log.i("passord", passordInput)
+            passord.error = "Passord må inneholde minst 1 eller flere store bokstaver og tall"
+            return false
+        }*/
+        else if(!passordInput.equals(passordInputBekreft)){
+            Log.i("passord", "matcher ikke")
+            passordBekreft.error = "Passord matcher ikke"
+            return false
+        }
+        else{
+            passord.error = null
+            passordBekreft.error = null
+            return true
+        }
+    }
+
+    fun validateFornavn(): Boolean{
+        val fornavnInput = fornavn.editText?.text.toString().trim()
+        if(fornavnInput.isEmpty()){
+            fornavn.error = "Fornavn kan ikke være tomt"
+            return false
+        }
+        else{
+            fornavn.error = null
+            return true
+        }
+    }
+
+    fun validateEtternavn(): Boolean{
+        val etternavnInput = etternavn.editText?.text.toString().trim()
+        if(etternavnInput.isEmpty()){
+            etternavn.error = "Etternavn kan ikke være tomt"
+            return false
+        }
+        else{
+            etternavn.error = null
+            return true
+        }
+    }
+
+    fun validateFdato(): Boolean{
+        if(fdato.text.isEmpty()){
+            fdato.hint = "Du må velge en fødselsdato"
+            fdato.setHintTextColor(resources.getColor(R.color.error))
+            return false
+        }
+        else if (fdato.text.toString().split("/".toRegex())[2].toInt() > Calendar.getInstance().get(Calendar.YEAR) - 13){
+            fdato.hint = "Du er ikke gammel nok til å registrere deg"
+            fdato.text = null
+            fdato.setHintTextColor(resources.getColor(R.color.error))
+            return false
+        }
+        else{
+            Log.i("confirm", "ok")
+            return true
+        }
+    }
+
+    fun validateInput() {
+
+        if(!validateEmail() or !validatePassord() or !validateFornavn() or !validateEtternavn() or !validateFdato()){
+            return
+        }
+        Log.i("update", "oppretter bruker")
+        auth.createUserWithEmailAndPassword(email.editText?.text.toString(), passord.editText?.text.toString())
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val bruker = Profil(
+                        email.editText?.text.toString(),
+                        fornavn.editText?.text.toString(),
+                        etternavn.editText?.text.toString(),
+                        fdato.text.toString()
+                    )
+                    database.addBruker(user?.uid!!, bruker)
+                    reload()
+                } else{
+                    Toast.makeText(
+                        context,
+                        "Autentisering feilet: ${task.exception.toString()}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+    }
+
+
+    fun datepicker(view: View) {
+        datoKnapp = view.findViewById(R.id.registrer_bruker_dp_icon)
+        fdato = view.findViewById(R.id.registrer_bruker_datepicker)
+
+        fdato.setOnClickListener {
+            displayDatePicker()
+        }
+
+        datoKnapp.setOnClickListener {
+            displayDatePicker()
+        }
+    }
+
+    fun displayDatePicker(){
 
         //Vet ikke om du har funnet koden her, Truls?
         //https://stackoverflow.com/questions/45842167/how-to-use-datepickerdialog-in-kotlin   ???
-
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
 
-        datoKnapp.setOnClickListener {
-
-            val dpd = DatePickerDialog(requireContext(), DatePickerDialog.OnDateSetListener{ view, mYear, mMonth, mDay ->
-                fdato.setText(""+mDay+"/"+mMonth+"/"+mYear)
-            }, year, month, day)
-            dpd.show()
-        }
+        val dpd = DatePickerDialog(requireContext(), DatePickerDialog.OnDateSetListener{ view, mYear, mMonth, mDay ->
+            fdato.setText(""+mDay+"/"+mMonth+"/"+mYear)
+        }, year, month, day)
+        dpd.show()
     }
-
-    /*private fun isDatoValid(): Boolean{
-
-        val day = fdato_day.text.toString().toInt()
-        val month = fdato_month.text.toString().toInt()
-        val year = fdato_year.text.toString().toInt()
-
-        if(year > 1900 && year < 2021-16)
-            if(year % 4 == 0){
-                // Skuddår
-                if(month in 1..12)
-                    when(month){
-                        2 -> return day in 1..29
-                        1,3,5,7,8,10,12 -> return day in 1..31
-                        else -> return day in 1..30
-                    }
-            }
-            else{ // Ikke skuddår
-                if(month in 1..12)
-                    when(month){
-                        2 -> return day in 1..28
-                        1,3,5,7,8,10,12 -> return day in 1..31
-                        else -> return day in 1..30
-                    }
-            }
-
-        return false
-    }*/
-
     private fun reload(){
         activity?.finish()
         startActivity(activity?.intent)
@@ -169,4 +216,5 @@ class RegistrerFragment : Fragment(R.layout.fragment_registrer) {
     companion object {
         fun newInstance(param1: String, param2: String) = RegistrerFragment()
     }
+
 }
